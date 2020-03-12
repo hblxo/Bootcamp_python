0a05f44b0e2fade124f63bbbba659a68b8ed53d2
hbouchet <hbouchet@student.42.fr>
2020-03-12 17:00:47 +0100

    Update : day02/ex03

--- day02/ex03/csvreader.py
+++ day02/ex03/csvreader.py
@@ -29,19 +29,50 @@
 class CsvReader():
     def __init__(self, filename, sep=',', header=False, skip_top=0, skip_bottom=0):
         self.sep = sep
-        self.header = header
+        self.header = None
+        self.headerFlag = header
         self.skip_top = skip_top
         self.skip_bottom = skip_bottom
         self.filename = filename
         self.file = None
+        self.data = []
+        # print('------ Enter init method ------')
 
     def __enter__(self):
-        self.file = open(self.filename)
-        return self.file
+        # print('------ Enter enter method ------')
+        try:
+            self.file = open(self.filename)
+            if self.headerFlag:
+                self.header = self.file.readline().split(self.sep)
+                self.skip_top += 1
+            with open(self.filename) as my_file:
+                lines_count = (sum(1 for _ in my_file))
+            row = 0
+            for i in range(self.skip_top, lines_count - self.skip_bottom):
+                line = list(
+                    filter(None, self.file.readline().strip().split(self.sep)))
+                if row == 0:
+                    row = len(line)
+                assert row == len(line)
+                self.data.append(line)
+        except (OSError, IOError) as e:
+            print(e)
+        except:
+            print("ERROR")
+            return None
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        # print('------ Enter exit method ------')
         self.file.close()
 
+    def getdata(self):
+        # print('------ Enter getdata method ------')
+        return self.data
+
+    def getheader(self):
+        # print('------ Enter getheader method ------')
+        return self.header
 # Usually the separator in csv files is the ,, but we want to be able to change this parameter. You can also tell the class to skip lines at the top and the bottom, and also to keep the first line as a header if header is True.
 
 # The file shouldn't be corrupted (line with too many elements), if it's corrupted return None. You have to handle the case file not found.
--- day02/ex03/test.py
+++ day02/ex03/test.py
@@ -2,12 +2,15 @@
 from csvreader import CsvReader
 
 if __name__ == "__main__":
-    with CsvReader('good.csv') as file:
+    with CsvReader('resources/good.csv', header=True) as file:
+        if file == None:
+            print("File is corrupted")
         data = file.getdata()
         header = file.getheader()
+        # print(data)
 
 
 if __name__ == "__main__":
-    with CsvReader('bad.csv') as file:
+    with CsvReader('resources/bad.csv') as file:
         if file == None:
             print("File is corrupted")
