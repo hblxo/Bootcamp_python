010adf782e84fb1fa732d87a962b601e0c2c4451
hbouchet <hbouchet@student.42.fr>
2020-03-12 20:46:28 +0100

    Add : day03/ex01

---
+++ day03/ex01/ImageProcessor.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib
+
+
+class ImageProcessor():
+    def __init__(self):
+        pass
+
+    def load(self, path):
+        pass
+
+    def display(self, array):
+        pass
